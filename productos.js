19b32bb83255ceca3cf816827bd2d648cc712e37
Valeria Roxana Laguna <valerialaguna0@gmail.com>
2026-05-01 15:07:40 -0300

    Crear el array de productos

---
+++ productos.js
@@ -0,0 +1,10 @@
+const productos = [
+    { id: 1, nombre: "Auriculares Bluetooth", precio: 50000, categoria: "electronicos", enStock: true },
+    { id: 2, nombre: "Cuaderno A4", precio: 5000, categoria: "articulos escolares", enStock: true },
+    { id: 3, nombre: "Reloj Inteligente", precio: 80000, categoria: "accesorios", enStock: true },
+    { id: 4, nombre: "Calculadora Científica", precio: 25000, categoria: "articulos escolares", enStock: false },
+    { id: 5, nombre: "Mouse Inalámbrico", precio: 15000, categoria: "electronicos", enStock: true },
+    { id: 6, nombre: "Mochila Urbana", precio: 40000, categoria: "accesorios", enStock: true },
+    { id: 7, nombre: "Teclado Mecánico", precio: 95000, categoria: "electronicos", enStock: false },
+    { id: 8, nombre: "Set de Marcadores", precio: 8000, categoria: "articulos escolares", enStock: true }
+];
